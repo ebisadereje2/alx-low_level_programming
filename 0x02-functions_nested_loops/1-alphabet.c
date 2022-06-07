c099741419edb2f288e6854831443be8c66e884b
ebisa dereje <ebisadereje2@gmail.com>
2022-06-07 07:39:18 -0700

    lower casee

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the lowercase alphabet once
+ *
+ * Return : Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
