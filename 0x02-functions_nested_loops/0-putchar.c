e259d6f6b76272a4586d7498cdc32e2b963741c1
ebisa dereje <ebisadereje2@gmail.com>
2022-06-07 04:38:34 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *pointer = "_putchar";
+
+	while (*pointer)
+	{
+		_putchar(*pointer);
+		pointer++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
