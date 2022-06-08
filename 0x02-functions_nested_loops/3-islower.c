fea154fe15dac937b659f34fcec58d59e7eace8f
ebisa dereje <ebisadereje2@gmail.com>
2022-06-08 00:03:34 -0700

    islowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *@c: single letter input
+ * Return: Always 1 OR 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c > 97)
+		return (1);
+	else
+		return (0);
+
+}
