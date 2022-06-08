c976c574d49924b0e76d9707922f74901c774862
ebisa dereje <ebisadereje2@gmail.com>
2022-06-07 23:49:48 -0700

    10 times print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{00
+		char c;
+
+		for(c ='a'; c <='z'; c++)
+		{
+			_putchar(c);
+		}
+		_puchar('\n');
+	
+	}
+}
